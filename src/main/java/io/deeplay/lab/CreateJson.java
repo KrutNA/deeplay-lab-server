d5647f8a945a7152cf814c8d2c897c07afca7693
Jjagorka <jagr68033@mail.ru>
2022-04-30 19:56:44 +0300

    add CreateJson

---
+++ src/main/java/io/deeplay/lab/CreateJson.java
@@ -0,0 +1,28 @@
+package io.deeplay.lab;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateJson {
+
+    public static void createJson(ArrayList<Location> locations) throws Exception {
+        var mapper = new ObjectMapper();
+
+        mapper.writeValue(Paths.get("Output_file.json").toFile(), locations);
+    }
+}
+
+record Location(
+        String locationName,
+        List<OurUnit> ourUnits
+) {
+}
+
+record OurUnit(
+        String name,
+        int locatePosition
+) {
+}
