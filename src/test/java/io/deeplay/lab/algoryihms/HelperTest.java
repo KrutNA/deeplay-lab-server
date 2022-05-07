de1545a3e6492a3978f50dec0ac821742bced9b9
Jjagorka <jagr68033@mail.ru>
2022-05-08 00:32:37 +0300

    add HelperTest

--- src/test/java/io/deeplay/lab/algoryihms/HelperTest.java
+++ src/test/java/io/deeplay/lab/algoryihms/HelperTest.java
@@ -1,7 +1,8 @@
-package io.deeplay.lab.algorythms;
+package io.deeplay.lab.algoryihms;
 
+import io.deeplay.lab.algorithm.Helper;
 import io.deeplay.lab.data.Location;
-import io.deeplay.lab.data.LocationReduce;
+import io.deeplay.lab.data.ReduceLocation;
 import io.deeplay.lab.data.Unit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,14 +15,14 @@ import java.util.Map;
 
 
 class HelperTest {
-    private LocationReduce firstLocation;
-    private LocationReduce secondLocation;
-    private LocationReduce thirdLocation;
-    private LocationReduce fourthLocation;
-    private LocationReduce fifthLocation;
-    private LocationReduce sixthLocation;
-    private LocationReduce seventhLocation;
-    private LocationReduce eighthLocation;
+    private ReduceLocation firstLocation;
+    private ReduceLocation secondLocation;
+    private ReduceLocation thirdLocation;
+    private ReduceLocation fourthLocation;
+    private ReduceLocation fifthLocation;
+    private ReduceLocation sixthLocation;
+    private ReduceLocation seventhLocation;
+    private ReduceLocation eighthLocation;
 
     private Unit opponentUnit;
     private Unit ourUnit;
@@ -41,42 +42,43 @@ class HelperTest {
         opponentsUnit.put(1, opponentUnit);
         opponentsUnit.put(2, opponentUnit);
 
-        firstLocation = new LocationReduce(1, (short) 6, opponentsUnit);
 
-        secondLocation = firstLocation.copy();
+        firstLocation = new ReduceLocation(1, (short) 6, opponentsUnit);
+
+        secondLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsSecondLocation = new HashMap<>();
         ourUnitsSecondLocation.put(3, ourUnit);
         secondLocation.setOurUnits(ourUnitsSecondLocation);
 
-        thirdLocation = firstLocation.copy();
+        thirdLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsThirdLocation = new HashMap<>();
         ourUnitsThirdLocation.put(4, ourUnit);
         thirdLocation.setOurUnits(ourUnitsThirdLocation);
 
-        fourthLocation = firstLocation.copy();
+        fourthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsFourthLocation = new HashMap<>();
         ourUnitsFourthLocation.put(5, ourUnit);
         fourthLocation.setOurUnits(ourUnitsFourthLocation);
 
-        fifthLocation = firstLocation.copy();
+        fifthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsFifthLocation = new HashMap<>();
         ourUnitsFifthLocation.put(3, ourUnit);
         ourUnitsFifthLocation.put(4, ourUnit);
         fifthLocation.setOurUnits(ourUnitsFifthLocation);
 
-        sixthLocation = firstLocation.copy();
+        sixthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsSixthLocation = new HashMap<>();
         ourUnitsSixthLocation.put(3, ourUnit);
         ourUnitsSixthLocation.put(5, ourUnit);
         sixthLocation.setOurUnits(ourUnitsSixthLocation);
 
-        seventhLocation = firstLocation.copy();
+        seventhLocation = firstLocation.clone();
         Map<Integer,Unit> OurUnitsSeventhLocation = new HashMap<>();
         OurUnitsSeventhLocation.put(4, ourUnit);
         OurUnitsSeventhLocation.put(5, ourUnit);
         seventhLocation.setOurUnits(OurUnitsSeventhLocation);
 
-        eighthLocation = firstLocation.copy();
+        eighthLocation = firstLocation.clone();
         Map<Integer,Unit> OurUnitsEighthLocation = new HashMap<>();
         OurUnitsEighthLocation.put(3, ourUnit);
         OurUnitsEighthLocation.put(4, ourUnit);
@@ -105,42 +107,42 @@ class HelperTest {
         opponentsUnit.put(1, opponentUnit);
         opponentsUnit.put(3, opponentUnit);
 
-        firstLocation = new LocationReduce(1, (short) 6, opponentsUnit);
+        firstLocation = new ReduceLocation(1, (short) 6, opponentsUnit);
 
-        secondLocation = firstLocation.copy();
+        secondLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsSecondLocation = new HashMap<>();
         ourUnitsSecondLocation.put(2, ourUnit);
         secondLocation.setOurUnits(ourUnitsSecondLocation);
 
-        thirdLocation = firstLocation.copy();
+        thirdLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsThirdLocation = new HashMap<>();
         ourUnitsThirdLocation.put(4, ourUnit);
         thirdLocation.setOurUnits(ourUnitsThirdLocation);
 
-        fourthLocation = firstLocation.copy();
+        fourthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsFourthLocation = new HashMap<>();
         ourUnitsFourthLocation.put(5, ourUnit);
         fourthLocation.setOurUnits(ourUnitsFourthLocation);
 
-        fifthLocation = firstLocation.copy();
+        fifthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsFifthLocation = new HashMap<>();
         ourUnitsFifthLocation.put(2, ourUnit);
         ourUnitsFifthLocation.put(4, ourUnit);
         fifthLocation.setOurUnits(ourUnitsFifthLocation);
 
-        sixthLocation = firstLocation.copy();
+        sixthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsSixthLocation = new HashMap<>();
         ourUnitsSixthLocation.put(2, ourUnit);
         ourUnitsSixthLocation.put(5, ourUnit);
         sixthLocation.setOurUnits(ourUnitsSixthLocation);
 
-        seventhLocation = firstLocation.copy();
+        seventhLocation = firstLocation.clone();
         Map<Integer,Unit> OurUnitsSeventhLocation = new HashMap<>();
         OurUnitsSeventhLocation.put(4, ourUnit);
         OurUnitsSeventhLocation.put(5, ourUnit);
         seventhLocation.setOurUnits(OurUnitsSeventhLocation);
 
-        eighthLocation = firstLocation.copy();
+        eighthLocation = firstLocation.clone();
         Map<Integer,Unit> OurUnitsEighthLocation = new HashMap<>();
         OurUnitsEighthLocation.put(2, ourUnit);
         OurUnitsEighthLocation.put(4, ourUnit);
@@ -172,7 +174,7 @@ class HelperTest {
         opponentsUnit.put(4, opponentUnit);
         opponentsUnit.put(5, opponentUnit);
 
-        firstLocation = new LocationReduce(1, (short) 6, opponentsUnit);
+        firstLocation = new ReduceLocation(1, (short) 6, opponentsUnit);
 
         List<Location> expectedLocations = Helper.converterGeneralCase(firstLocation);
 
@@ -188,20 +190,20 @@ class HelperTest {
         opponentsUnit.put(1, opponentUnit);
         opponentsUnit.put(2, opponentUnit);
 
-        firstLocation = new LocationReduce(1, (short) 6, opponentsUnit);
+        firstLocation = new ReduceLocation(1, (short) 6, opponentsUnit);
 
-        secondLocation = firstLocation.copy();
+        secondLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsSecondLocation = new HashMap<>();
         ourUnitsSecondLocation.put(3,ourUnit);
         secondLocation.setOurUnits(ourUnitsSecondLocation);
 
-        thirdLocation = firstLocation.copy();
+        thirdLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsThirdLocation = new HashMap<>();
         ourUnitsThirdLocation.put(3,ourUnit);
         ourUnitsThirdLocation.put(4,ourUnit);
         thirdLocation.setOurUnits(ourUnitsThirdLocation);
 
-        fourthLocation = firstLocation.copy();
+        fourthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsFourthLocation = new HashMap<>();
         ourUnitsFourthLocation.put(3,ourUnit);
         ourUnitsFourthLocation.put(4,ourUnit);
@@ -216,30 +218,38 @@ class HelperTest {
         actual.add(thirdLocation);
         actual.add(fourthLocation);
 
+        for (Location i : expectedLocations){
+            System.out.println(i.getOurUnits());
+        }
+        System.out.println("///////////////////////////////////////");
+        for (Location i : actual){
+            System.out.println(i.getOurUnits());
+        }
+
         Assertions.assertEquals(expectedLocations, actual);
     }
 
     @Test
-    //Если все места заняты опонентами
+    //Если рассадка опонентов непоследовательная
     public void testConverterOurCaseNotSequentialSeating() throws Exception {
         opponentsUnit.put(0, opponentUnit);
         opponentsUnit.put(1, opponentUnit);
         opponentsUnit.put(3, opponentUnit);
 
-        firstLocation = new LocationReduce(1, (short) 6, opponentsUnit);
+        firstLocation = new ReduceLocation(1, (short) 6, opponentsUnit);
 
-        secondLocation = firstLocation.copy();
+        secondLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsSecondLocation = new HashMap<>();
         ourUnitsSecondLocation.put(2,ourUnit);
         secondLocation.setOurUnits(ourUnitsSecondLocation);
 
-        thirdLocation = firstLocation.copy();
+        thirdLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsThirdLocation = new HashMap<>();
         ourUnitsThirdLocation.put(2,ourUnit);
         ourUnitsThirdLocation.put(4,ourUnit);
         thirdLocation.setOurUnits(ourUnitsThirdLocation);
 
-        fourthLocation = firstLocation.copy();
+        fourthLocation = firstLocation.clone();
         Map<Integer,Unit> ourUnitsFourthLocation = new HashMap<>();
         ourUnitsFourthLocation.put(2,ourUnit);
         ourUnitsFourthLocation.put(4,ourUnit);
@@ -255,11 +265,19 @@ class HelperTest {
         actual.add(fourthLocation);
 
 
+        for (Location i : expectedLocations){
+            System.out.println(i.getOurUnits());
+        }
+        System.out.println("///////////////////////////////////////");
+        for (Location i : actual){
+            System.out.println(i.getOurUnits());
+        }
+
         Assertions.assertEquals(expectedLocations, actual);
     }
 
     @Test
-    //Если рассадка опонентов непоследовательная
+    // Если все места заняты опонентами
     public void testConverterOurCaseNoPlaceForSeating() throws Exception {
         opponentsUnit.put(0, opponentUnit);
         opponentsUnit.put(1, opponentUnit);
@@ -268,12 +286,21 @@ class HelperTest {
         opponentsUnit.put(4, opponentUnit);
         opponentsUnit.put(5, opponentUnit);
 
-        firstLocation = new LocationReduce(1, (short) 6, opponentsUnit);
+        firstLocation = new ReduceLocation(1, (short) 6, opponentsUnit);
 
         List<Location> expectedLocations = Helper.converterOurCase(firstLocation);
 
         ArrayList<Location> actual = new ArrayList<>();
         actual.add(firstLocation);
+
+        for (Location i : expectedLocations){
+            System.out.println(i.getOurUnits());
+        }
+        System.out.println("///////////////////////////////////////");
+        for (Location i : actual){
+            System.out.println(i.getOurUnits());
+        }
+
 
         Assertions.assertEquals(expectedLocations, actual);
     }
