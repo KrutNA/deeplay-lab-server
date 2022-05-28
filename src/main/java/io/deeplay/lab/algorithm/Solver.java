7d3886d406d0b62a1f6dd55e55735996a4113d9d
Krutko Nikita / KrutNA <krutna@pm.me>
2022-05-28 16:35:42 +0600

    Implemented interface for solver and created base for solver based on weghts.

---
+++ src/main/java/io/deeplay/lab/algorithm/Solver.java
@@ -0,0 +1,8 @@
+package io.deeplay.lab.algorithm;
+
+import io.deeplay.lab.data.SolverInput;
+import io.deeplay.lab.data.SolverResult;
+
+public interface Solver {
+    SolverResult[] solve(SolverInput input);
+}
---
+++ src/main/java/io/deeplay/lab/algorithm/WeightedSolver.java
@@ -0,0 +1,11 @@
+package io.deeplay.lab.algorithm;
+
+import io.deeplay.lab.data.SolverInput;
+import io.deeplay.lab.data.SolverResult;
+
+public class WeightedSolver implements Solver {
+    @Override
+    public SolverResult[] solve(SolverInput input) {
+        return null;
+    }
+}
