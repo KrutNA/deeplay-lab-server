e85c36f5ac2252dddcb64a1ebfbe15a2e3def1ea
Krutko Nikita / KrutNA <krutna@pm.me>
2022-05-29 21:21:27 +0600

    Removed unnecessary and breaking method from Helper.

--- src/main/java/io/deeplay/lab/algorithm/Helper.java
+++ src/main/java/io/deeplay/lab/algorithm/Helper.java
@@ -1,40 +1,11 @@
 package io.deeplay.lab.algorithm;
 
-import com.google.common.collect.Sets;
 import io.deeplay.lab.data.Location;
 import io.deeplay.lab.data.Unit;
 
 import java.util.*;
 
 public class Helper {
-    public static List<Location> converterGeneralCase(Location location) {
-        // NEED TO BE TESTED
-        List<Location> locationConverted = new ArrayList<>();
-
-        Set<Integer> positions = new HashSet<>(location.getFreePositions());
-        List<Set<Integer>> combinations = new ArrayList<>();
-
-        for (int i = 0; i < location.getFreePositions().size()+1; i++) {
-            combinations.addAll(Sets.combinations(positions, i));
-
-        }
-
-        for (var comb : combinations) {
-            Location newLocation = location.clone();
-            Map<Integer, Unit> units = new HashMap<>();
-
-            for (var pos : comb) {
-                units.put(pos, new Unit(0, null));
-            }
-
-            newLocation.setOurUnits(units);
-            locationConverted.add(newLocation);
-        }
-
-
-        return locationConverted;
-    }
-
 
     public static List<Location> converterOurCase(Location location) {
         List<Location> locationConverted = new ArrayList<>();
